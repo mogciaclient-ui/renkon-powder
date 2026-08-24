07cd8cf0d37687b8f57209e18b18ca7499457f39
mogciaclient-ui <mogcia.client@gmail.com>
2026-08-24 19:01:46 +0900

    Add LINE friend button to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="fixed inset-x-0 top-0 z-50 bg-white/92 backdrop-blur-sm">
-      <div className="wide-container h-[76px] flex items-center">
+      <div className="wide-container h-[76px] flex items-center justify-between">
         <a href="#top" className="flex items-center gap-3">
           <Image
             src="/images/renkon-logo.png"
@@ -17,6 +17,14 @@ export default function Header() {
             <br />
             パウダー
           </span>
+        </a>
+        <a href="https://lin.ee/OhfkLHA">
+          <img
+            src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png"
+            alt="友だち追加"
+            height="36"
+            className="h-9 w-auto"
+          />
         </a>
       </div>
     </header>
